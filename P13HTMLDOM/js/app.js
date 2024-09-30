972074812aaf978599cec5eb607c2466993074b3
seffmeister <ismail.sefin@gmail.com>
2024-09-30 11:28:05 +0200

    variuos things from class

---
+++ P13HTMLDOM/js/app.js
@@ -0,0 +1,20 @@
+
+const textField0 = document.getElementById("text0");
+
+textField0.innerHTML = average(22, 44);
+
+const headerText5 = document.getElementById("header5");
+headerText5.innerHTML = 'Åk hem till Linköping';
+headerText5.style.color = 'red';
+
+const button0 = document.getElementById("button0");
+button0.style.backgroundColor = 'red';
+
+
+function average(firstNumber, secondNumber) {
+  return (firstNumber + secondNumber) / 2;
+}
+
+textField0.innerHTML = 'Hej + ' ' Bitch';
+
+
